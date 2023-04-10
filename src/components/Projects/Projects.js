47aa96357e2084b7c304d95ad68bfbe0148a3531
sumanth-cs <sumanthshetty698@gmail.com>
2023-04-10 13:40:34 +0530

    links added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import codeEditor from "../../img/codeEditorImg.png"
 import insta from "../../img/instaImg.webp"
 import movieCity from "../../img/movieCityImg.gif"
 import txtEditor from "../../img/txtEditorImg.jpeg"
-// import {Link} from "react-router-dom"
+import calculator from "../../img/calculatorImg.png"
 
 export default function Projects() {
   return (
@@ -35,38 +35,57 @@ export default function Projects() {
         </SwiperSlide>
 
         <SwiperSlide>
+        <a href="https://vocal-kitsune-c7cf1a.netlify.app" target="_blank" rel="noreferrer">
           <img src={movieCity} alt="" />
           <div className='txt'>
-            Movie Booking App
+            Movie City App
           </div>
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
+        <a href="https://benevolent-hamster-5649d9.netlify.app" target="_blank" rel="noreferrer">
           <img src={codeEditor} alt="" />
           <div className='txt'>
             Code Editor
           </div>
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
+        <a href="https://bored-cod-lab-coat.cyclic.app" target="_blank" rel="noreferrer">
           <img src={insta} alt="" />
           <div className='txt'>
             Social Media App
           </div>
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
+        <a href="https://whatsapp-clone-17602.web.app" target="_blank" rel="noreferrer">
           <img src={chat} alt="" />
           <div className='txt'>
             Messaging App
           </div>
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
+        <a href="https://shimmering-klepon-9a6094.netlify.app" target="_blank" rel="noreferrer">
           <img src={txtEditor} alt="" />
           <div className='txt'>
             Text Editor
           </div>
+          </a>
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <a href="https://resilient-tartufo-97e8b0.netlify.app" target="_blank" rel="noreferrer">
+          <img src={calculator} alt="" />
+          <div className='txt'>
+            Basic Calculator
+          </div>
+          </a>
         </SwiperSlide>
         <div className="blur p_blur2" style={{ background: "#edd0ff",top:"18rem",left:"7s0rem" }}></div>
 
